ae1a513bb8d9e94ac6c06db950575f3d547ef2b0
skjaldbaka17 <skjaldbaka17@gmail.com>
2021-06-20 16:28:39 +0000

    Some EC2 server configs resources

--- setupEc2.sh
+++ setupEc2.sh
@@ -4,6 +4,9 @@
 echo DATABASE_URL=
 read database_url
 
+echo Server Running on PORT=
+read port
+
 #Perform a quick update on your instance:
 sudo yum update -y
 
@@ -22,10 +25,13 @@ git version
 git clone https://github.com/Skjaldbaka17/quotes-api.git
 cd quotes-api
 echo "DATABASE_URL=$database_url" > .env
+echo "PORT=$port" >> .env
 #start server https://www.tecmint.com/run-linux-command-process-in-background-detach-process/
 nohup make server &
 cd ..
 
+#Need to create new nginx.conf in /etc/nginx looking like the nginx.conf in this root!
+echo "Need to create new nginx.conf in /etc/nginx looking like the nginx.conf in this root!"
 
 #Cloning db-setup from rep, do the setup from local comuter (if using free-tier or something with less than 4GB storage)
 #git clone https://github.com/Skjaldbaka17/setup-quotes-db.git
