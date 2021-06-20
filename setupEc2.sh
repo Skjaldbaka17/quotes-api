351bada2a325de40acbf4bc80f6a139cdc7c4e3f
skjaldbaka17 <skjaldbaka17@gmail.com>
2021-06-20 14:37:30 +0000

    Set port to default http i.e. 80 and added a .sh file for setting up the environment for the EC2 instance

---
+++ setupEc2.sh
@@ -0,0 +1,46 @@
+#Create an EC2 instance with Amazon Linux 2 with internet access
+#Connect to your instance using putty
+#Get the DB_url
+echo DATABASE_URL=
+read database_url
+
+#Perform a quick update on your instance:
+sudo yum update -y
+
+#Install Golang
+sudo yum install -y golang
+
+#Install git in your EC2 instance
+sudo yum install git -y
+
+sudo amazon-linux-extras install nginx1 -y
+
+#Check git version
+git version
+
+#Cloning quotes-api / server from repo
+git clone https://github.com/Skjaldbaka17/quotes-api.git
+cd quotes-api
+echo "DATABASE_URL=$database_url" > .env
+#start server https://www.tecmint.com/run-linux-command-process-in-background-detach-process/
+nohup make server &
+cd ..
+
+
+#Cloning db-setup from rep, do the setup from local comuter (if using free-tier or something with less than 4GB storage)
+#git clone https://github.com/Skjaldbaka17/setup-quotes-db.git
+#cd setup-quotes-db
+#echo DATABASE_URL=
+#read database_url
+#echo "export DATABASE_URL=$database_url" > .env
+#make setup
+#cd ..
+
+
+
+#if this fails maybe need to kill running http
+#sudo fuser -k 80/tcp
+#And then run start nginx again
+
+# Get What process is using port 80
+#fuser 80/tcp
